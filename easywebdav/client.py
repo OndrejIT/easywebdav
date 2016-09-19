a99b04c6e0f4ed4eff40a2d60235000a1ea91260
Ondřej Bárta <git@ondrej.it>
2016-09-19 16:11:22 +0200

    fix python3

--- easywebdav/client.py
+++ easywebdav/client.py
@@ -6,12 +6,13 @@ from collections import namedtuple
 
 py_majversion, py_minversion, py_revversion = platform.python_version_tuple()
 
-if py_majversion == '2':
+if py_majversion == "2":
     from httplib import responses as HTTP_CODES
     from urlparse import urlparse
 else:
     from http.client import responses as HTTP_CODES
     from urllib.parse import urlparse
+    basestring = (str, bytes)
 
 DOWNLOAD_CHUNK_SIZE_BYTES = 1 * 1024 * 1024
 
@@ -23,24 +24,24 @@ class ConnectionFailed(WebdavException):
 
 
 def codestr(code):
-    return HTTP_CODES.get(code, 'UNKNOWN')
+    return HTTP_CODES.get(code, "UNKNOWN")
 
 
-File = namedtuple('File', ['name', 'size', 'mtime', 'ctime', 'contenttype'])
+File = namedtuple("File", ["name", "size", "mtime", "ctime", "contenttype"])
 
 
 def prop(elem, name, default=None):
-    child = elem.find('.//{DAV:}' + name)
+    child = elem.find(".//{DAV:}" + name)
     return default if child is None else child.text
 
 
 def elem2file(elem):
     return File(
-        prop(elem, 'href'),
-        int(prop(elem, 'getcontentlength', 0)),
-        prop(elem, 'getlastmodified', ''),
-        prop(elem, 'creationdate', ''),
-        prop(elem, 'getcontenttype', ''),
+        prop(elem, "href"),
+        int(prop(elem, "getcontentlength", 0)),
+        prop(elem, "getlastmodified", ""),
+        prop(elem, "creationdate", ""),
+        prop(elem, "getcontenttype", ""),
     )
 
 
@@ -60,26 +61,26 @@ class OperationFailed(WebdavException):
         self.expected_code = expected_code
         self.actual_code = actual_code
         operation_name = self._OPERATIONS[method]
-        self.reason = 'Failed to {operation_name} "{path}"'.format(**locals())
+        self.reason = "Failed to {operation_name} {path}".format(**locals())
         expected_codes = (expected_code,) if isinstance(expected_code, Number) else expected_code
-        expected_codes_str = ", ".join('{0} {1}'.format(code, codestr(code)) for code in expected_codes)
+        expected_codes_str = ", ".join("{0} {1}".format(code, codestr(code)) for code in expected_codes)
         actual_code_str = codestr(actual_code)
-        msg = '''\
+        msg = """\
 {self.reason}.
   Operation     :  {method} {path}
   Expected code :  {expected_codes_str}
-  Actual code   :  {actual_code} {actual_code_str}'''.format(**locals())
+  Actual code   :  {actual_code} {actual_code_str}""".format(**locals())
         super(OperationFailed, self).__init__(msg)
 
 class Client(object):
     def __init__(self, host, port=0, auth=None, username=None, password=None,
-                 protocol='http', verify_ssl=True, path=None, cert=None):
+                 protocol="http", verify_ssl=True, path=None, cert=None):
         if not port:
-            port = 443 if protocol == 'https' else 80
-        self.baseurl = '{0}://{1}:{2}'.format(protocol, host, port)
+            port = 443 if protocol == "https" else 80
+        self.baseurl = "{0}://{1}:{2}".format(protocol, host, port)
         if path:
-            self.baseurl = '{0}/{1}'.format(self.baseurl, path)
-        self.cwd = '/'
+            self.baseurl = "{0}/{1}".format(self.baseurl, path)
+        self.cwd = "/"
         self.session = requests.session()
         self.session.verify = verify_ssl
         self.session.stream = True
@@ -102,7 +103,7 @@ class Client(object):
 
     def _get_url(self, path):
         path = str(path).strip()
-        if path.startswith('/'):
+        if path.startswith("/"):
             return self.baseurl + path
         return "".join((self.baseurl, self.cwd, path))
 
@@ -110,24 +111,24 @@ class Client(object):
         path = path.strip()
         if not path:
             return
-        stripped_path = '/'.join(part for part in path.split('/') if part) + '/'
-        if stripped_path == '/':
+        stripped_path = "/".join(part for part in path.split("/") if part) + "/"
+        if stripped_path == "/":
             self.cwd = stripped_path
-        elif path.startswith('/'):
-            self.cwd = '/' + stripped_path
+        elif path.startswith("/"):
+            self.cwd = "/" + stripped_path
         else:
             self.cwd += stripped_path
 
     def mkdir(self, path, safe=False):
         expected_codes = 201 if not safe else (201, 301, 405)
-        self._send('MKCOL', path, expected_codes)
+        self._send("MKCOL", path, expected_codes)
 
     def mkdirs(self, path):
-        dirs = [d for d in path.split('/') if d]
+        dirs = [d for d in path.split("/") if d]
         if not dirs:
             return
-        if path.startswith('/'):
-            dirs[0] = '/' + dirs[0]
+        if path.startswith("/"):
+            dirs[0] = "/" + dirs[0]
         old_cwd = self.cwd
         try:
             for dir in dirs:
@@ -142,27 +143,27 @@ class Client(object):
             self.cd(old_cwd)
 
     def rmdir(self, path, safe=False):
-        path = str(path).rstrip('/') + '/'
+        path = str(path).rstrip("/") + "/"
         expected_codes = 204 if not safe else (204, 404)
-        self._send('DELETE', path, expected_codes)
+        self._send("DELETE", path, expected_codes)
 
     def delete(self, path):
-        self._send('DELETE', path, 204)
+        self._send("DELETE", path, 204)
 
     def upload(self, local_path_or_fileobj, remote_path):
         if isinstance(local_path_or_fileobj, basestring):
-            with open(local_path_or_fileobj, 'rb') as f:
+            with open(local_path_or_fileobj, "rb") as f:
                 self._upload(f, remote_path)
         else:
             self._upload(local_path_or_fileobj, remote_path)
 
     def _upload(self, fileobj, remote_path):
-        self._send('PUT', remote_path, (200, 201, 204), data=fileobj)
+        self._send("PUT", remote_path, (200, 201, 204), data=fileobj)
 
     def download(self, remote_path, local_path_or_fileobj):
-        response = self._send('GET', remote_path, 200, stream=True)
+        response = self._send("GET", remote_path, 200, stream=True)
         if isinstance(local_path_or_fileobj, basestring):
-            with open(local_path_or_fileobj, 'wb') as f:
+            with open(local_path_or_fileobj, "wb") as f:
                 self._download(f, response)
         else:
             self._download(local_path_or_fileobj, response)
@@ -171,18 +172,18 @@ class Client(object):
         for chunk in response.iter_content(DOWNLOAD_CHUNK_SIZE_BYTES):
             fileobj.write(chunk)
 
-    def ls(self, remote_path='.'):
-        headers = {'Depth': '1'}
-        response = self._send('PROPFIND', remote_path, (207, 301), headers=headers)
+    def ls(self, remote_path="."):
+        headers = {"Depth": "1"}
+        response = self._send("PROPFIND", remote_path, (207, 301), headers=headers)
 
         # Redirect
         if response.status_code == 301:
-            url = urlparse(response.headers['location'])
+            url = urlparse(response.headers["location"])
             return self.ls(url.path)
 
         tree = xml.fromstring(response.content)
-        return [elem2file(elem) for elem in tree.findall('{DAV:}response')]
+        return [elem2file(elem) for elem in tree.findall("{DAV:}response")]
 
     def exists(self, remote_path):
-        response = self._send('HEAD', remote_path, (200, 301, 404))
+        response = self._send("HEAD", remote_path, (200, 301, 404))
         return True if response.status_code != 404 else False
